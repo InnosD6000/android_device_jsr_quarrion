2c5581d1c13e7cfca4552d744074dacd248a6a3a
Ramswaroop Ghanshyam Somani <rsomani@cyngn.com>
2016-02-19 14:46:02 -0800

    quarrion: Don't push background services to higher adj.
    REF: TOMATOL-615, CRACKLING-831
    Change-Id: Ibe5ec4f1eaa4ba1912fb630290c1af68795ab2ba

--- product/stock.mk
+++ product/stock.mk
@@ -1,9 +1,3 @@
-# Enable B service adj transition by default
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.bservice_enable=true \
-    ro.sys.fw.bservice_limit=5 \
-    ro.sys.fw.bservice_age=5000
-
 # Optimal dex2oat threads for faster app installation
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=4
