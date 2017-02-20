4e675e7babd2225b1e5db482b93c216f7b90c1c4
Louis Popi <theh2o64@gmail.com>
2017-02-20 09:47:23 +0100

    quarrion: Use CAF Memory configuration script
    Change-Id: I9f38d9176c646f7951203f626b82dfbd251ed3b9

--- product/init.mk
+++ product/init.mk
@@ -3,6 +3,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc \
     init.qcom.rc \
+    init.qcom.mem.sh \
     init.qcom.power.rc \
     init.qcom.ssr.rc \
     init.recovery.qcom.rc \
