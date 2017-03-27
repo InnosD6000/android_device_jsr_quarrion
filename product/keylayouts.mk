cb2ba510f920fa05008b8ca3355155a9011304fa
Ilya Lebedev <ashuli2003@gmail.com>
2017-03-27 16:40:41 +0300

    quarrion: keylayout from stock
    Change-Id: Idcd36d73c014dcb845b636e3156e93c2302a7281

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,4 +1,5 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/cyttsp4_btn.kl:system/usr/keylayout/cyttsp4_btn.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
