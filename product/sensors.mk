16988a8d494399aa7d5ef8f711f3b79a35d645e6
Ilya Lebedev <ashuli2003@gmail.com>
2017-03-28 19:58:42 +0300

    quarrion: Remove sensors HAL * We will use a proprietary sensor HAL * For FIX sensors
    Change-Id: Ie337e0f6e9813c020c4a3fb8cb559174ce2d9a4b

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,9 +1,3 @@
-# Sensors
-PRODUCT_PACKAGES += \
-    libcalmodule_common \
-    LsmCalibration \
-    sensors.msm8916
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
