b487b86d94fed0d5458de2f2b56632e7bd3d76d3
Ilya Lebedev <ashuli2003@gmail.com>
2017-03-28 20:59:47 +0300

    quarrion: Use wlan module to fix it
    Change-Id: I35b9adc2061433c61404e388d69365f207398023

--- board/qcom-wlan.mk
+++ board/qcom-wlan.mk
@@ -1,3 +1,7 @@
 # Wifi
 CONFIG_EAP_PROXY := qmi
 CONFIG_EAP_PROXY_DUAL_SIM := true
+
+# Use module because use stock kernel for now
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/pronto/pronto_wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "pronto_wlan"
