f8ed18afafb14f201f0814e98a25af14707928f6
Ilya Lebedev <ashuli2003@gmail.com>
2017-03-28 23:20:12 +0300

    quarrion: Remove synaptics keylayout
    Change-Id: Ifdcfdccf8653547d37839217a80f61a895745138

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,5 +1,3 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
