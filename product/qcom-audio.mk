550646dd9dc6584e3e3efe0639648f1410d6ce25
Ilya Lebedev <ashuli2003@gmail.com>
2017-03-31 06:18:15 +0300

    quarrion: Add sound_trigger's configs
    Change-Id: Idaf4f779f546221cc754f6edcfb3e918e786125d

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -19,6 +19,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_skul.xml:system/etc/mixer_paths_skul.xml \
     $(LOCAL_PATH)/audio/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_info.xml:system/etc/sound_trigger_mixer_paths_info.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
