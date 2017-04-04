13c4be6d0898e356c6e01b9758586c13ec5db744
Ilya Lebedev <ashuli2003@gmail.com>
2017-04-04 10:16:32 +0300

    quarrion: Don't require dt.img for prebuilt kernel
    Change-Id: I883ffaa3bd2b94e1fe883a8d6980158521a27626

--- board/kernel.mk
+++ board/kernel.mk
@@ -12,3 +12,8 @@ ENABLE_CPUSETS := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_CUSTOM_BOOTIMG_MK := device/jsr/quarrion/mkbootimg.mk
 TARGET_PREBUILT_KERNEL := device/jsr/quarrion/zImage
+
+# Don't require dt.img for prebuilt kernel
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_SEPARATED_DT := false
+TARGET_KERNEL_APPEND_DTB := true
