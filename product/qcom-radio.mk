0132dbfc926c0dfc7f1da680aa86576596c77078
pirej <pirej30@yahoo.com>
2017-04-21 23:37:18 +0200

    quarrion: VoLTE support (2/2)
    Based on the tomato part from vendor_volte by Louis Popi
    
    Change-Id: I2c49c981bace30b87c4ca4cc6ba013964e3ec25c

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -33,3 +33,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.telephony.default_network=9,1
+
+# IMSEnabler
+PRODUCT_PACKAGES += \
+   IMSEnabler \
+   libshim_parcel \
+   libshim_boringssl \
+   libshims_camera \
+   libshims_ims
