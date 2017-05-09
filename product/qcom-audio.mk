f6a38583d3cd9f56d5e6e9407af2307bb263edfb
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-09 16:38:18 +0300

    quarrion: Fix separate
    Change-Id: I798b1cf8fbc69f3b0dd7a5b8c57948386de67f9d

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -40,7 +40,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
 	frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 # Properties
