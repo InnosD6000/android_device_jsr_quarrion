315d04c095ef85ee004b6f0fda2626267f9b91cf
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-16 14:37:44 +0300

    quarrion: More props...
    Change-Id: I8efdf996130d3d6921ca3c1d1bf1396f8873f615

--- product/stock.mk
+++ product/stock.mk
@@ -14,8 +14,39 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Add for camera (add by hanjue)
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.tintless=enable
+    persist.camera.tintless=enable \
+	persist.denoise.process.plates=0
 
 # Property to enable narrow search range for video encoding
 PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.narrow.searchrange=1
+
+# System prop for custom hls enhancements
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.hls.enhancements=false
+
+# System props for the MM modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    media.msm8939hw=0 \
+    media.msm8929hw=0 \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true
+
+# Codecs:(PARSER_)AAC AC3 AMR_NB AMR_WB ASF AVI DTS FLV 3GP 3G2 MKV MP2PS MP2TS MP3 OGG QCP WAV FLAC DIVX DIVXHD
+PRODUCT_PROPERTY_OVERRIDES += \
+    mm.enable.qcom_parser=3407871
+
+# System property for cabl
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0
+
+# property to enable VDS WFD solution
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.hwc.enable_vds=1
