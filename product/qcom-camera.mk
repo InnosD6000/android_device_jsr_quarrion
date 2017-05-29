bd6159d70ed68807174a53ad255a877aae4f9193
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-29 07:23:02 +0300

    quarrion: More cam. permissions
    Change-Id: I057114366b0d64c2646690a22e92dbb0d7c63813

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -7,7 +7,9 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml\
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml\
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
