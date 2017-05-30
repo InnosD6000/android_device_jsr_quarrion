972aebaeb3a4f4080d148c2cccad4cfdc83652cf
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-30 21:02:09 +0300

    quarrion: Use wlan instead pronto_wlan
    Change-Id: I320abd09167234fd817c76bbb30cd2ee245e0715

--- board/qcom-wlan.mk
+++ board/qcom-wlan.mk
@@ -3,8 +3,8 @@ CONFIG_EAP_PROXY := qmi
 CONFIG_EAP_PROXY_DUAL_SIM := true
 
 # Use module because use stock kernel for now
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/pronto/pronto_wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "pronto_wlan"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # From common
 BOARD_HAS_QCOM_WLAN := true
