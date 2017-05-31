5441f672ed7e3060b483513ca59a962115431b02
Ilya Lebedev <ashuli2003@gmail.com>
2017-05-31 10:44:24 +0300

    quarrion: Kang keyhandler from Elephone P9000 * Thanks to @Deepflex
    Change-Id: I49643c4aea5fe9c164c71257baca058b33b18ee3

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,3 +1,12 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
+# Keyhandler
+PRODUCT_PACKAGES += \
+    com.cyanogenmod.keyhandler
+
+PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+
+# Never dexopt the keyhandler
+$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
