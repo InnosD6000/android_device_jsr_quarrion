63a5da29039214de107329bd16a565e38638faf8
Ilya Lebedev <ashuli2003@gmail.com>
2017-06-02 22:23:19 +0300

    quarrion: Kang sensors HAL from DEXP Ixion M LTE 5" * Because mlte5 has cm13.0 on stock kernel * May be this HAL compatible with our kernel
    Change-Id: Idc17462a3b0e447b708184b01b4c72ceebcaa799

--- product/sensors.mk
+++ product/sensors.mk
@@ -6,3 +6,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Sensor HAL
+PRODUCT_PACKAGES += \
+    calmodule.cfg \
+    libcalmodule_common \
+    sensors.msm8916
