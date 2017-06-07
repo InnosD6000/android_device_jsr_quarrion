1fa2f3fb2d475353ddd49192acfaac071885463b
Ilya Lebedev <ashuli2003@gmail.com>
2017-06-07 23:21:36 +0300

    Revert "quarrion: VoLTE support (2/2)"
    This reverts commit 0132dbfc926c0dfc7f1da680aa86576596c77078.

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -33,11 +33,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.telephony.default_network=9,1
-
-# IMSEnabler
-PRODUCT_PACKAGES += \
-   IMSEnabler \
-   libshim_parcel \
-   libshim_boringssl \
-   libshims_camera \
-   libshims_ims
